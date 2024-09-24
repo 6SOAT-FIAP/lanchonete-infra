a6e3b792c7f85b93624df9371e950adb8cdec266
Luhan Lacerda <luhanlacerda@gmail.com>
2024-09-24 18:57:37 -0300

    chore: ajuste comando push ecr image

--- eks/ecr-lanchonete-api.tf
+++ eks/ecr-lanchonete-api.tf
@@ -40,6 +40,7 @@ resource "null_resource" "push_image_lanchonete_api_to_ecr" {
     #command = "aws ecr get-login-password | docker login --username AWS --password-stdin 638385053556.dkr.ecr.us-east-1.amazonaws.com && docker build -t 638385053556.dkr.ecr.us-east-1.amazonaws.com/lanchonete-api:latest ../../ && docker push 638385053556.dkr.ecr.us-east-1.amazonaws.com/lanchonete-api:latest"
 
     command     = <<-EOT
+      cd ..
       aws ecr get-login-password | docker login --username AWS --password-stdin 638385053556.dkr.ecr.us-east-1.amazonaws.com
       mkdir -p ./temp_repo_lanchonete_api  # Cria diretório temporário exclusivo para a api da lanchonete
       cd ./temp_repo_lanchonete_api || exit 1
@@ -47,7 +48,7 @@ resource "null_resource" "push_image_lanchonete_api_to_ecr" {
       cd ./lanchonete-api-repo || exit 1
       docker build -t 638385053556.dkr.ecr.us-east-1.amazonaws.com/lanchonete-api:latest .
       docker push 638385053556.dkr.ecr.us-east-1.amazonaws.com/lanchonete-api:latest
-      rm -rf ./temp_repo_payment
+      rm -rf ./temp_repo_lanchonete_api
     EOT
     working_dir = path.module
   }
