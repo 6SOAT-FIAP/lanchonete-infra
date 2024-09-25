3965a263c9f9eda9994f9d9d78afd62ba1520b72
Luhan Lacerda <luhanlacerda@gmail.com>
2024-09-25 19:19:23 -0300

    chore infra

--- eks/eks.tf
+++ eks/eks.tf
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "lanchonete-api" {
     min_size     = 1
   }
 
-  instance_types = "t3.medium"
+  instance_types = ["t3.medium"]
   disk_size      = 20
   ami_type       = "AL2_x86_64"
   depends_on     = [aws_eks_cluster.lanchonete-api]
