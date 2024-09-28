b8c6da18e9aee7aa7fdd52e1b358f58d87f0d870
Luhan Lacerda <luhanlacerda@gmail.com>
2024-09-28 11:19:23 -0300

    chore: ajuste aws_subnets

--- eks/data.tf
+++ eks/data.tf
@@ -6,10 +6,13 @@ data "aws_vpc" "vpc" {
   cidr_block = "172.31.0.0/16"
 }
 
-data "aws_subnets" "subnets" {
-  for_each = toset(data.aws_subnets.subnets.id)
-  id       = each.value
+data "aws_subnets" "subnets"{
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
 }
+
 
 # Não funciona pq n temos permissão no aws academy
 #data "aws_iam_role" "labRole"{
