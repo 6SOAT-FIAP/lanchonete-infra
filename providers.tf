d30f0d1c0df2373f22f68a7e6a9daa875a2f31ac
Luhan Lacerda <luhanlacerda@gmail.com>
2024-10-02 18:15:18 -0300

    chore: ajustando provider k8s

--- providers.tf
+++ providers.tf
@@ -23,6 +23,7 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.lanchonete_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.lanchonete_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.lanchonete_cluster_auth.token
+  load_config_file       = false
 }
 
 # provider "kubernetes" {
