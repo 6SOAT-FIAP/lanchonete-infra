e1ac7321616b8cb31b9c98f3cc44b2a5f9676e1f
Luhan Lacerda <luhanlacerda@gmail.com>
2024-10-03 00:09:40 -0300

    chore: incluind helm

--- providers.tf
+++ providers.tf
@@ -27,14 +27,18 @@ data "aws_eks_cluster_auth" "cluster" {
   name = "eks-lanchonete-api"
 }
 
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
 }
 
 provider "kubernetes" {
+  experiments {
+    manifest_resource = true
+  }
   #  host                   = aws_eks_cluster.lanchonete_cluster.endpoint
   #  cluster_ca_certificate = base64decode(aws_eks_cluster.lanchonete_cluster.certificate_authority[0].data)
   #  token                  = data.aws_eks_cluster_auth.lanchonete_cluster_auth.token
